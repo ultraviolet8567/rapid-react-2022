5dc8bf24649b32b7236dd6ed16f833d4873d66a6
Kal Draganov <kaldraganov05@gmail.com>
2022-03-04 21:10:36 -0500

    Commands for changing flywheel speeds

---
+++ src/main/java/frc/robot/commands/FlywheelOneDecrease.java
@@ -0,0 +1,41 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.Shooter;
+
+public class FlywheelOneDecrease extends CommandBase {
+    private final Shooter m_shooter;
+    
+    public FlywheelOneDecrease(Shooter subsystem) {
+        m_shooter = subsystem;
+        addRequirements(m_shooter);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        Constants.shooter1Speed -= 0.001;
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public boolean runsWhenDisabled() {
+        return false;
+    }
+}
